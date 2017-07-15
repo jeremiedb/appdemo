456a970f1d23359cf460f3feee686ff0a2252d54
jeremiedb <jeremie.desgagne.bouchard@gmail.com>
2017-07-15 03:15:35 -0400

    test rating

---
+++ R/rating.R
@@ -0,0 +1,13 @@
+#' Rating
+#' Basic hello world function to be called from the demo app
+#' 
+#' @export
+#' @param mean Expected mean
+rating <- function(mean=20){
+  if(mean <=0){
+    stop("Must be positive mean")
+  }
+  list(
+    rating = paste0("rating: ", rnorm(1, mean = mean))
+  )
+}
