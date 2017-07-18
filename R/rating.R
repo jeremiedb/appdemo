48387dbdf267aac5d6ab922a53b9936c43139344
jeremiedb <jeremie.desgagne.bouchard@gmail.com>
2017-07-17 23:21:07 -0400

    add sd to rating

--- R/rating.R
+++ R/rating.R
@@ -3,11 +3,11 @@
 #' 
 #' @export
 #' @param mean Expected mean
-rating <- function(mean=20){
+rating <- function(mean=20, sd=1){
   if(mean <=0){
     stop("Must be positive mean")
   }
   list(
-    rating = paste0("rating: ", rnorm(1, mean = mean))
+    rating = paste0("rating: ", rnorm(1, mean = mean, sd = sd))
   )
 }
